87dedf06b198f6456cb8d6c3deed82b7faa14299
Aetra <valentin.hajek@etu.univ-lorraine.fr>
2020-09-25 08:55:36 +0200

    dinguerie

---
+++ src/TD1/UltrasonicSensor.java
@@ -0,0 +1,47 @@
+package TD1;
+
+import lejos.hardware.motor.Motor;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.SampleProvider;
+import lejos.utility.Delay;
+
+public class UltrasonicSensor {
+
+	//Robot Configuration
+	private static EV3UltrasonicSensor us1 = new EV3UltrasonicSensor(SensorPort.S4);
+
+	//Configuration
+	private static int HALF_SECOND = 500;
+
+	public static void main(String[] args) throws InterruptedException {
+
+		SampleProvider sp;
+
+		int distanceValue = 3;
+        Motor.C.setSpeed(110);
+        Motor.B.setSpeed(110);
+        sp = us1.getListenMode();
+		int sampleSize = sp.sampleSize();
+		float[] sample = new float[sampleSize];
+		sp.fetchSample(sample, 0);
+		Motor.B.forward();
+		Motor.C.forward();
+		int i=1;
+		while(distanceValue > 1) {
+
+			sp.fetchSample(sample, i);
+
+			distanceValue = (int)sample[i];
+			i++;
+
+			System.out.println(" Distance: {}" + distanceValue);
+
+			Delay.msDelay(2000);	
+
+	}
+		Motor.B.stop();
+		Motor.C.stop();
+		us1.close();
+}
+}
