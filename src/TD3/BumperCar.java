4fb8f60602ed30d384620d84d961d518cb2a5543
nicolas-carbonnier <nicoinferno55@gmail.com>
2020-09-25 09:35:36 +0200

    BumperCar + DriveForward

---
+++ src/TD3/BumperCar.java
@@ -0,0 +1,31 @@
+package TD3;
+
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.robotics.subsumption.Arbitrator;
+import lejos.robotics.subsumption.Behavior;
+
+public class BumperCar {
+
+	public static void main(String[] args) {
+		EV3TouchSensor touchSensor = new EV3TouchSensor(SensorPort.S1);
+		float[] sample = new float[] {0};
+		
+		Behavior driveForward = new DriveForward(); // Avancer
+		Behavior hitWall = new HitWall(touchSensor, sample); // Contourner
+		Behavior arreter = new Arreter(touchSensor); // Arreter
+		Behavior batterie = new CompBatt();
+		
+		Behavior[] behaviors = {
+				driveForward,
+				hitWall,
+				arreter,
+				batterie
+			};
+		
+		Arbitrator arby = new Arbitrator(behaviors);
+		((Arreter) arreter).setArby(arby);
+		arby.go();
+	}
+
+}
